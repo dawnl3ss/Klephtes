31b84a667a1e7882c659f8e4e9fe1fbf7f718a43
Neptune <52249849+Neptune-IT@users.noreply.github.com>
2022-05-17 17:53:51 +0200

    update [1.1.1] : changing requires

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . "/Kleptes/src/grab/KleptesGrab.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/Kleptes/src/provider/MySQL.php";
+require_once "src/grab/KleptesGrab.php";
+require_once "src/provider/MySQL.php";
 
 (new KleptesGrab($_SERVER, new MySQL()))->insert_data();
